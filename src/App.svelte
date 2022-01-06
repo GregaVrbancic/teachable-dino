459728b5d0b9d4035e7429280f96a280a935a43d
Grega Vrbančič <grega.vrbancic@gmail.com>
2022-01-06 12:50:02 +0100

    Attempt to fix background urls

--- src/App.svelte
+++ src/App.svelte
@@ -193,15 +193,19 @@
 		height: 200px;
 		/* border: 1px solid black; */
 		margin: 200px auto 0 auto;
-		background-image: url("./floor.png");
-		background-repeat-y: no-repeat;
+		background-image:
+			url("/floor.png"),
+			url("..public/floor.png");
+		background-repeat: repeat-x;
     	background-position: bottom;
 	}
 
 	#character {
 		width: 50px;
 		height: 50px;
-		background-image: url("./dino.gif");
+		background-image:
+			url("/dino.gif"),
+			url("../public/dino.gif");
 		position: relative;
 		top: 150px;
 		z-index: -1;
@@ -219,7 +223,9 @@
 	}
 
 	#cloud {
-		background-image: url("./cloud.png");
+		background-image:
+			url("/cloud.png"),
+			url("../public/cloud.png");
 		height: 16px;
 		width: 50px;
 		position: relative;
@@ -229,7 +235,9 @@
 	}
 
 	#bat {
-		background-image: url("./bat.png");
+		background-image:
+			url("/bat.png"),
+			url("../public/bat.png");
 		width: 50px;
 		height: 38px;
 		position: relative;
@@ -239,7 +247,9 @@
 	}
 
 	#cactus {
-		background-image: url("./cactus.png");
+		background-image:
+			url("/cactus.png"),
+			url("../public/cactus.png");
 		width: 20px;
 		height: 37px;
 		position: relative;
